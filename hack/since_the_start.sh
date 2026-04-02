6d9848426b22ec625ca5a8d767c0117218118907
Kingdon B <kingdon@urmanac.com>
2026-04-02 12:14:50 -0400

    update history export (feb, mar)
    Signed-off-by: Kingdon B <kingdon@urmanac.com>

--- hack/since_the_start.sh
+++ hack/since_the_start.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 source .env.local
 export EARLY_API_KEY EARLY_API_SECRET
-OUTPUT_FILE=2024_11_history.csv ruby ./export.rb 2024 11
-OUTPUT_FILE=2024_12_history.csv ruby ./export.rb 2024 12
-OUTPUT_FILE=2025_01_history.csv ruby ./export.rb 2025 01
-OUTPUT_FILE=2025_02_history.csv ruby ./export.rb 2025 02
-OUTPUT_FILE=2025_03_history.csv ruby ./export.rb 2025 03
-OUTPUT_FILE=2025_04_history.csv ruby ./export.rb 2025 04
-OUTPUT_FILE=2025_05_history.csv ruby ./export.rb 2025 05
-OUTPUT_FILE=2025_06_history.csv ruby ./export.rb 2025 06
-OUTPUT_FILE=2025_07_history.csv ruby ./export.rb 2025 07
-OUTPUT_FILE=2025_08_history.csv ruby ./export.rb 2025 08
-OUTPUT_FILE=2025_09_history.csv ruby ./export.rb 2025 09
-OUTPUT_FILE=2025_10_history.csv ruby ./export.rb 2025 10
+# OUTPUT_FILE=2024_11_history.csv ruby ./export.rb 2024 11
+# OUTPUT_FILE=2024_12_history.csv ruby ./export.rb 2024 12
+# OUTPUT_FILE=2025_01_history.csv ruby ./export.rb 2025 01
+# OUTPUT_FILE=2025_02_history.csv ruby ./export.rb 2025 02
+# OUTPUT_FILE=2025_03_history.csv ruby ./export.rb 2025 03
+# OUTPUT_FILE=2025_04_history.csv ruby ./export.rb 2025 04
+# OUTPUT_FILE=2025_05_history.csv ruby ./export.rb 2025 05
+# OUTPUT_FILE=2025_06_history.csv ruby ./export.rb 2025 06
+# OUTPUT_FILE=2025_07_history.csv ruby ./export.rb 2025 07
+# OUTPUT_FILE=2025_08_history.csv ruby ./export.rb 2025 08
+# OUTPUT_FILE=2025_09_history.csv ruby ./export.rb 2025 09
+# OUTPUT_FILE=2025_10_history.csv ruby ./export.rb 2025 10
+# OUTPUT_FILE=2025_11_history.csv ruby ./export.rb 2025 11
+# OUTPUT_FILE=2025_12_history.csv ruby ./export.rb 2025 12
+# OUTPUT_FILE=2026_01_history.csv ruby ./export.rb 2026 01
+OUTPUT_FILE=2026_02_history.csv ruby ./export.rb 2026 02
+OUTPUT_FILE=2026_03_history.csv ruby ./export.rb 2026 03
+# OUTPUT_FILE=2026_04_history.csv ruby ./export.rb 2026 04
+# OUTPUT_FILE=2026_05_history.csv ruby ./export.rb 2026 05
+# OUTPUT_FILE=2026_06_history.csv ruby ./export.rb 2026 06
+# OUTPUT_FILE=2026_07_history.csv ruby ./export.rb 2026 07
+# OUTPUT_FILE=2026_08_history.csv ruby ./export.rb 2026 08
+# OUTPUT_FILE=2026_09_history.csv ruby ./export.rb 2026 09
+# OUTPUT_FILE=2026_10_history.csv ruby ./export.rb 2026 10
